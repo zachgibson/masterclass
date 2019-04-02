0e3c60a281b96cb0f70d7181a280f05b3730514d
Zach Gibson <zach.gibson@taillight.com>
2019-04-02 13:35:26 -0500

    Pull local data into its own file and add unique keys to components

---
+++ data.js
@@ -0,0 +1,202 @@
+export default [
+    {
+        uri: require('./images/deadmau5.jpg'),
+        firstName: 'Deadmau5',
+        lastName: ''
+    },
+    {
+        uri: require('./images/judy-blume.jpg'),
+        firstName: 'Judy',
+        lastName: 'Blume'
+    },
+    {
+        uri: require('./images/james-patterson.jpg'),
+        firstName: 'James',
+        lastName: 'Patterson'
+    },
+    {
+        uri: require('./images/spike-lee.jpg'),
+        firstName: 'Spike',
+        lastName: 'Lee'
+    },
+    {
+        uri: require('./images/christina-aguilera.jpg'),
+        firstName: 'Christina',
+        lastName: 'Aguilera'
+    },
+    {
+        uri: require('./images/martin-scorsese.jpg'),
+        firstName: 'Martin',
+        lastName: 'Scorsese'
+    },
+    {
+        uri: require('./images/shonda-rhimes.jpg'),
+        firstName: 'Shonda',
+        lastName: 'Rhimes'
+    },
+    {
+        uri: require('./images/aaron-sorkin.jpg'),
+        firstName: 'Aaron',
+        lastName: 'Sorkin'
+    },
+    {
+        uri: require('./images/marc-jacobs.jpg'),
+        firstName: 'Marc',
+        lastName: 'Jacobs'
+    },
+    {
+        uri: require('./images/r-l-stine.jpg'),
+        firstName: 'R.L.',
+        lastName: 'Stine'
+    },
+    {
+        uri: require('./images/chris-hadfield.jpg'),
+        firstName: 'Chris',
+        lastName: 'Hadfield'
+    },
+    {
+        uri: require('./images/gordon-ramsay.jpg'),
+        firstName: 'Gordon',
+        lastName: 'Ramsay'
+    },
+    {
+        uri: require('./images/annie-leibovitz.jpg'),
+        firstName: 'Annie',
+        lastName: 'Leibovitz'
+    },
+    {
+        uri: require('./images/daniel-negreanu.jpg'),
+        firstName: 'Daniel',
+        lastName: 'Negreanu'
+    },
+    {
+        uri: require('./images/garry-kasparov.jpg'),
+        firstName: 'Garry',
+        lastName: 'Kasparov'
+    },
+    {
+        uri: require('./images/frank-gehry.jpg'),
+        firstName: 'Frank',
+        lastName: 'Gehry'
+    },
+    {
+        uri: require('./images/samuel-l-jackson.jpg'),
+        firstName: 'Samuel L.',
+        lastName: 'Jackson'
+    },
+    {
+        uri: require('./images/ken-burns.jpg'),
+        firstName: 'Ken',
+        lastName: 'Burns'
+    },
+    {
+        uri: require('./images/margaret-atwood.jpg'),
+        firstName: 'Margaret',
+        lastName: 'Atwood'
+    },
+    {
+        uri: require('./images/stephen-curry.jpg'),
+        firstName: 'Stephen',
+        lastName: 'Curry'
+    },
+    {
+        uri: require('./images/malcolm-gladwell.jpg'),
+        firstName: 'Malcolm',
+        lastName: 'Gladwell'
+    },
+    {
+        uri: require('./images/thomas-keller.jpg'),
+        firstName: 'Thomas',
+        lastName: 'Keller'
+    },
+    {
+        uri: require('./images/helen-mirren.jpg'),
+        firstName: 'Helen',
+        lastName: 'Mirren'
+    },
+    {
+        uri: require('./images/herbie-hancock.jpg'),
+        firstName: 'Herbie',
+        lastName: 'Hancock'
+    },
+    {
+        uri: require('./images/steve-martin.jpg'),
+        firstName: 'Steve',
+        lastName: 'Martin'
+    },
+    {
+        uri: require('./images/hans-zimmer.jpg'),
+        firstName: 'Hans',
+        lastName: 'Zimmer'
+    },
+    {
+        uri: require('./images/armin-van-buuren.jpg'),
+        firstName: 'Armin Van',
+        lastName: 'Buuren'
+    },
+    {
+        uri: require('./images/alice-waters.jpg'),
+        firstName: 'Alice',
+        lastName: 'Waters'
+    },
+    {
+        uri: require('./images/ron-howard.jpg'),
+        firstName: 'Ron',
+        lastName: 'Howard'
+    },
+    {
+        uri: require('./images/david-mamet.jpg'),
+        firstName: 'David',
+        lastName: 'Mamet'
+    },
+    {
+        uri: require('./images/dr-jane-goodall.jpg'),
+        firstName: 'Dr. Jane',
+        lastName: 'Goodall'
+    },
+    {
+        uri: require('./images/wolfgang-puck.jpg'),
+        firstName: 'Wolfgang',
+        lastName: 'Puck'
+    },
+    {
+        uri: require('./images/diane-von-furstenberg.jpg'),
+        firstName: 'Diane Von',
+        lastName: 'Furstenberg'
+    },
+    {
+        uri: require('./images/bob-woodward.jpg'),
+        firstName: 'Bob',
+        lastName: 'Woodward'
+    },
+    {
+        uri: require('./images/judd-apatow.jpg'),
+        firstName: 'Judd',
+        lastName: 'Apatow'
+    },
+    {
+        uri: require('./images/serena-williams.jpg'),
+        firstName: 'Serena',
+        lastName: 'Williams'
+    },
+    {
+        uri: require('./images/werner-herzog.jpg'),
+        firstName: 'Werner',
+        lastName: 'Herzog'
+    },
+    {
+        uri: require('./images/usher.jpg'),
+        firstName: 'usher',
+        lastName: ''
+    },
+    {
+        uri: require('./images/reba-mcentire.jpg'),
+        firstName: 'Reba',
+        lastName: 'Mcentire'
+    },
+    {
+        uri: require('./images/stephen-king.jpg'),
+        firstName: 'Stephen',
+        lastName: 'King'
+    }
+];
